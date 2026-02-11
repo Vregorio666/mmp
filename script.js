4a615a57ba987ba66d357328197bb48ae433af3e
Vregorio666 <117214724+Vregorio666@users.noreply.github.com>
2026-02-11 12:27:38 -0300

    mapa base

--- script.js
+++ script.js
@@ -4,11 +4,13 @@
 var mapa = L.map('mapa').setView([-39.95, -72.8], 9);
 
 // Capa OSM
-var osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.{ext}', {
-    minZoom: 9,
-    maxZoom: 20,
-    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    ext: 'png'
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 0,
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+osm.addTo(mapa);
 });
 osm.addTo(mapa);
 
@@ -3224,4 +3226,5 @@ el deterioro de su salud.
  <h4>
  `)
 });
+
 
